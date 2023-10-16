addc2a943752f6d1cd65c32ee946682970063865
Tomislav Celic <celer.tomislav@gmail.com>
2023-10-16 19:14:52 +0200

    Fixed formatting

--- FizzBuzz/fizz_buzzer.cpp
+++ FizzBuzz/fizz_buzzer.cpp
@@ -4,9 +4,11 @@
 std::string fizz_buzzer::create_fizz_string(const uint32_t fizz_number)
 {
 	std::stringstream stream = { };
+
 	if (fizz_number % 3 == 0)
 		stream << "Fizz";
 	if (fizz_number % 5 == 0)
 		stream << "Buzz";
+
 	return stream.str();
 }
